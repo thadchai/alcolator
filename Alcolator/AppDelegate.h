eb61e2ee1730543adcac6db3823f1c167000b6bf
Thadchai Sattayatam <tsatta@gmail.com>
2015-01-07 21:47:38 -0600

    Initial Commit

---
+++ Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Work on 1/7/15.
+//  Copyright (c) 2015 Thadchai Sattayatam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Work on 1/7/15.
+//  Copyright (c) 2015 Thadchai Sattayatam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
